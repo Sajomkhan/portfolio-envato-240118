6013fe11d801283c0278416d5971bc5bcf3e8489
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2024-01-29 12:36:48 +0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,27 @@
+import { socialmedias } from "../data/iconData";
 
 const Footer = () => {
   return (
-    <div className="container">Footer</div>
-  )
-}
+    <div className="container flex flex-col md:flex-row-reverse gap-4 justify-between items-center my-8">
+      <div className="flex gap-6">
+        {socialmedias.map((socialmedia, index) => {
+          const { icon, address } = socialmedia;
+          return (
+            <div key={index}>
+              <a href={address} target="blank">
+                <img
+                  className="h-5 w-5 bg-gray-200 rounded-md shadow-md hover:bg-teal-300 hover:scale-110 duration-300"
+                  src={icon}
+                  alt="Icon Image"
+                />
+              </a>
+            </div>
+          );
+        })}
+      </div>
+      <p className="text-sm text-gray-400">© 2024 copyright all right reserved by Ajom</p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
